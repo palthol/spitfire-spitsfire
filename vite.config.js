ff6591aade8513237c704558ab6fdd051f892493
Palthol <Ilikericeat@gmail.com>
2024-11-19 18:09:13 -0500

    Modified config and json files. Added a tool that allows us to deploy our website inside of github pages.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',    // Keep the output folder as '../dist' if you prefer to have the build at the root level
     emptyOutDir: true     // This will ensure the dist folder is cleared on every build
-  }
-})
+  },
+  base: '/spitfire-spitsfire/' // Ensure correct base path for GitHub Pages
+});
