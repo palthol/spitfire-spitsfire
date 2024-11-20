59c1c27b8458c494cab818e8408ac880f8e844e7
Palthol <Ilikericeat@gmail.com>
2024-11-19 19:24:18 -0500

    fixes on file paths

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,17 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   build: {
-    outDir: '../dist',    // Keep the output folder as '../dist' if you prefer to have the build at the root level
-    emptyOutDir: true     // This will ensure the dist folder is cleared on every build
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        html: './src/html.html',
+        git: './src/git.html',
+        css: './src/css.html',
+        js: './src/js.html'
+      }
+    }
   },
   base: '/spitfire-spitsfire/' // Ensure correct base path for GitHub Pages
 });
